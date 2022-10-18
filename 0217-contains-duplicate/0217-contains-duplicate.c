ee3d4ec785495304797b025f3f715a4bf903354e
Mohit Savner <MohitSavner@users.noreply.github.com>
2022-10-18 16:46:16 +0530

    Time: 137 ms (77.27%), Space: 12.7 MB (81.47%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.c
+++ 0217-contains-duplicate/0217-contains-duplicate.c
@@ -1,13 +1,13 @@
 
 int cmp(const void *a, const void *b)
 {
-    return (*(int*)a - *(int*)b);
+    return (*(int*)a - *(int*)b); //Comparing two values if no difference then its Duplicate
 }
 
 bool containsDuplicate(int* nums, int numsSize){
     qsort(nums, numsSize, sizeof(int), cmp); //using quick sort where 2 indices are compared
     for (int i = 1; i < numsSize; i++){
-    if(nums[i] == nums[i-1])  //Comparing current and previous indices
+    if(nums[i] == nums[i-1])  //Comparing current and previous indices to get duplicate
     return true; 
     }
   return false;    
