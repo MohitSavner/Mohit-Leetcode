c66ec1bcc216674da1e9982979d8617bb792b650
Mohit Savner <MohitSavner@users.noreply.github.com>
2022-10-19 08:22:48 +0530

    Time: 2 ms (57.58%), Space: 5.8 MB (81.46%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.c
@@ -0,0 +1,35 @@
+/*
+  Method 1: Brute force approach
+    Time Complexity: O(n^2)
+    Space Complexity: O(1)
+    */
+    
+    int numIdenticalPairs(int* nums, int numsSize){
+    int counter = 0;
+    for(int i = 0; (i < numsSize); i++){ 
+        for(int j= i+1; j < numsSize ; j++){
+             if(nums[i] == nums[j] && i < j)
+            counter++ ;
+        }
+       
+    }
+    return counter;
+}
+    
+
+/*
+  Method 2: Optimized Approach
+    Time Complexity: O(n)
+    Space Complexity: O(n)
+
+
+int numIdenticalPairs(int* nums, int numsSize){
+    int gpair = (numsSize*(numsSize-1)/2);
+    for(int i = 0, j=i+1; (i < numsSize); i++){
+      
+    }
+       
+    }
+    return counter;
+}
+*/
