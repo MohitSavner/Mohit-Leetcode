f8b854026adfbe9f01236780de225d927e9f0aa4
Mohit Savner <MohitSavner@users.noreply.github.com>
2022-10-19 08:58:44 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (47.47%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.c
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.c
@@ -2,7 +2,7 @@
   Method 1: Brute force approach
     Time Complexity: O(n^2)
     Space Complexity: O(1)
-    */
+    
     
     int numIdenticalPairs(int* nums, int numsSize){
     int counter = 0;
@@ -15,21 +15,26 @@
     }
     return counter;
 }
-    
+*/ 
 
 /*
   Method 2: Optimized Approach
     Time Complexity: O(n)
     Space Complexity: O(n)
-
+*/
 
 int numIdenticalPairs(int* nums, int numsSize){
-    int gpair = (numsSize*(numsSize-1)/2);
-    for(int i = 0; (i < numsSize); i++){
-      
+  int Counter = 0;
+  unsigned short int CounterArray[101];
+    for(unsigned short int j=0 ; j < 101; j++){
+      CounterArray[j] = 0;
     }
-       
+    for(unsigned short int i = 0; i < numsSize; i++){
+      CounterArray[nums[i]]++;
     }
-    return counter;
+    for(unsigned short int i = 0; i < 101; i++){
+    unsigned short int res = (CounterArray[i]*(CounterArray[i]-1)/2); //Sum of n-1 number series {1,2,3,....n-1} formula
+    Counter += res;
+  }
+    return Counter;
 }
-*/
