9cfb572d7aba72e07360debd1de689a454c3fa5e
Mohit Savner <MohitSavner@users.noreply.github.com>
2022-10-19 16:17:33 +0530

    Time: 5 ms (94.46%), Space: 6.8 MB (54.70%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.c
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.c
@@ -1,13 +1,17 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
- */
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+**/
 
 int* runningSum(int* nums, int numsSize, int* returnSize){
-  int sum = 0;
-  *returnSize = numsSize;
-    for(int i = 0; i < numsSize; i++){
-      sum += nums[i];
-      nums[i] = sum ;
+  *returnSize = numsSize; //Deferencing to numsSize to avoid heap overflow
+/* Example:   
+      returnSize = 2;
+      int *arr = malloc(sizeof(int) * (*returnSize));   //Occcurs in main funtion. 
+*/
+  for(int i = 1; i < numsSize; i++){
+      nums[i] = nums[i-1] + nums[i];    //Adding 0th to 1st index and so on.. and returning to respective index of given array.
     }
   return nums;
 }
