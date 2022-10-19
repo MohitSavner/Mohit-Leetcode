a887557fbe4355983ca041656f03eb04eef117e9
Mohit Savner <MohitSavner@users.noreply.github.com>
2022-10-19 09:42:06 +0530

    Time: 2 ms (57.58%), Space: 5.7 MB (81.46%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.c
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.c
@@ -6,10 +6,10 @@
     
     int numIdenticalPairs(int* nums, int numsSize){
     int counter = 0;
-    for(int i = 0; i < numsSize; i++){ 
-        for(int j= i+1; j < numsSize ; j++){
-             if(nums[i] == nums[j] && i < j)
-            counter++ ;
+    for(int i = 0; i < numsSize; i++){ //first index i.e. 0th
+        for(int j= i+1; j < numsSize ; j++){ //Second index i.e. 1st ...
+             if(nums[i] == nums[j] && i < j)  //Comparing two indices with and i < j
+            counter++ ;  // Incrementing Counter
         }
        
     }
@@ -25,16 +25,16 @@
 
 int numIdenticalPairs(int* nums, int numsSize){
   int Counter = 0;
-  unsigned short int CounterArray[101];
+  unsigned short int CounterArray[101]; //Counter Array size is given as per Constraints
     for(unsigned short int j=0 ; j < 101; j++){
       CounterArray[j] = 0;
     }
-    for(unsigned short int i = 0; i < numsSize; i++){
-      CounterArray[nums[i]]++;
+    for(unsigned short int i = 0; i < numsSize; i++){ 
+      CounterArray[nums[i]]++; //Incrementing counter array as per array indices
     }
     for(unsigned short int i = 0; i < 101; i++){
     unsigned short int res = (CounterArray[i]*(CounterArray[i]-1)/2); //Sum of n-1 number series {1,2,3,....n-1} formula
-    Counter += res;
+    Counter += res; // Incrementing counter with iterating result 
   }
-    return Counter;
+    return Counter;   //Returing a counter as good pair in numbers
 }
