4866026b43463b0105cacb1ea1a1aad032de3802
Mohit Savner <MohitSavner@users.noreply.github.com>
2022-10-21 17:14:05 +0530

    Time: 6 ms (65.91%), Space: 5.6 MB (61.36%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.c
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.c
@@ -1,24 +1,19 @@
-
-int powof2(int n){
-  int i = 1;
-  while(i*2<= n){
-    i = i*2;
-  }
-  return i;
-}
-
+/*
+//Solution using recursion
+//n = 2 -> the winner index is (0 + k) % 2 (base-0)
+//n = 3 -> the winner index is (f(2) + k) % 3 (base-0)
+//with n -> the winner index is (f(n-1) + k) % n (base-0)
+//
 
 int findTheWinner(int n, int k){
     if(n == 1) return 1;
     return (findTheWinner(n-1, k) + k - 1) % n + 1;
 }
-
-/*
-int findTheWinner(int n, int k){
-  
-  
-  int res = powof2(n);
-  int l = n - res;
-  return (2*l+1);
-}
 */
+//Solution in Linear time
+int findTheWinner(int n, int k){
+    int result = 0;
+    for(int i = 1; i<=n; ++i)
+        result = (result + k) % i;
+    return result + 1;
+}
